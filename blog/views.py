bef0a58a8ff49b0395a8410d8ce7cf4e96450df4
git2chirag <ramnanichirag70@gmail.com>
2023-07-29 19:43:26 +0530

    'update'

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import post
 
-posts=[{'author':'DSPAPLU','title':'blog posts','content':'First Post content','date':'9/11'},{'author':'tom jerry','title':'FUN and GAmes','content':'2nd Post content','date':'26/11'}] #dummy data
 
 def home(request):
-    context={'posts':posts} #setting up posts
+    context={'posts':post.objects.all()} #setting up posts
     return render(request,'blog/home.html',context) # using render function instead of http response
 
 def about(request):
--- django_project/settings.py
+++ django_project/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
